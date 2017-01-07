deb63ab753ef3f93c2ebd1beb8af7ec27aec3e82
Pietro Boccaletto <pietro@Pietros-MacBook-Air.local>
2017-01-07 19:30:57 +0100

    This commit brings many changes to GeekBook. I will try to list few of the main features, but I mostly lost the count. 1. Added the possibility to re-compile all the webpages with arg -u ; 2. Moved main app outside the app directory that has been renamed engine ; 3. Now the app shows the time of the compiling of a certain file; so you know what you compiled ! 4. Disabled css file; 5. added function in post_html to change the normal html tag with bootstrap powered classes. 6. added an image above the list of the pages in the index, but doesn't show up. I need to fix this.

---
+++ engine/src/lib.py
@@ -0,0 +1,18 @@
+from geekbook.engine.conf import PATH_TO_IMG
+import re
+
+
+def get_image_path(l):
+    """Get image path for l(ine).
+
+    :rtype: string, line
+    """
+    rx = re.compile('\!\[\]\((?P<filename>.+)\)').search(l)
+
+    if rx:
+        path_new = '<a class="lightbox" href="' + PATH_TO_IMG + '/' + rx.group('filename') +'">'+\
+                   '<img src="' + PATH_TO_IMG + '/' + rx.group('filename') + \
+                   '" alt="" title=""></a> \n'
+        return path_new
+    else:
+        return l
---
+++ engine/src/make_index.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""Get the list of md with sys.stdint.read() and generate html index.html.
+The top of the html file is defined here, see the html variable.
+The second part is generated in the loop, per md make a link in the index.html"""
+import os
+import time
+import re
+
+from geekbook.engine.conf import PATH_TO_HTML, PATH_TO_TEMPLATE, PATH_HOMEPAGE, PATH_TO_MD
+
+
+class Index(object):
+    def __init__(self):
+        pass
+
+    def update(self, list_md):
+        """Update the index page"""
+        html = open(PATH_TO_TEMPLATE + '/head.html').read()
+
+        html = html.replace('{{ url_index }}', PATH_TO_HTML + '/' + 'index.html')
+
+        # @todo
+        html = html.replace('href="img/', 'href="' + PATH_TO_TEMPLATE + '/img/')
+        html = html.replace('src="img/', 'src="' + PATH_TO_TEMPLATE + '/img/')
+        html = html.replace('="lib/', '="' + PATH_TO_TEMPLATE + '/lib/')
+        html = html.replace('="css/', '="' + PATH_TO_TEMPLATE + '/css/')
+
+        # remove demo content
+        html = re.sub(r'<!-- start of demo -->.*<!-- end of demo -->', r'', html, flags=re.M | re.DOTALL)
+        ##
+
+        html += open(PATH_HOMEPAGE).read()
+        html += '<table style="width:100%">'
+
+        for l in list_md:
+            if l == 'imgs':
+                pass
+            else:
+                if l.strip():
+                    l = l.replace('.md', '')
+                    path = PATH_TO_HTML + '/' + l
+
+                    #if l.find('::')>=0:
+                    #    html += '<li class="table_of_content_h2"><a style="" href="' + path + '.html">' + l + '</a></li>'
+                    #else:
+                    html += '<tr><td><li class="index_list"><a class="index_list_a" href="' + path + '.html">' + l + '</a>' \
+                            + '<td><small>' + time.ctime(os.stat(os.path.join(PATH_TO_MD, l + '.md')).st_mtime) + '</small></td></li></tr>'
+
+        html += '</p>'
+
+        f = open(PATH_TO_HTML + 'index.html', 'w')
+        f.write(html)
+        f.close()
