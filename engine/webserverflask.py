64b465f9f96f9fed31808c76acbf7829e8f9dd7a
Marcin Magnus <mag_dex@o2.pl>
2017-01-10 12:48:53 +0100

    flask: make it nice

--- engine/webserverflask.py
+++ engine/webserverflask.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/edit/<post_id>')
-def edit(post_id):
-    # show the post with the given id, the id is an integer
-    os.system('open ../notes/' + post_id )
-    return 'edit %s' % post_id
+@app.route('/edit/<note_title>')
+def edit(note_title):
+    """Open a note with your edit"""
+    os.system('open ../notes/' + note_title)
+    return 'edit note: %s' % note_title
 
 if __name__ == "__main__":
     app.run()
