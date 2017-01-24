3c7bd2665e829a144d98842a67e1eb81eb9ad158
magnus <magnus@genesilico.pl>
2017-01-24 14:54:11 -0800

    fix #38 (fix ligthbox2)

--- engine/preprocessing.py
+++ engine/preprocessing.py
@@ -48,9 +48,7 @@ def get_image_path(text):
         rx = re.compile('\!\[\]\((?P<filename>.+)\)').search(l)
 
         if rx:
-            path_new = '<a data-lightbox="image" href="' + PATH_TO_IMG + '/' + rx.group('filename') +'">'+\
-                       '<img src="' + PATH_TO_IMG + '/' + rx.group('filename') + \
-                       '" alt="" title=""></a> \n'
+            path_new = '<a data-lightbox="note" href="' + PATH_TO_IMG + '/' + rx.group('filename') +'"><img src="' + PATH_TO_IMG + '/' + rx.group('filename') +'"></a> \n'
             return path_new
         else:
             return l
