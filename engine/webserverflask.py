17918386b28bc48dde2730c5ee87a836c5d69ff7
magnus <magnus@genesilico.pl>
2017-05-09 12:19:47 +0200

    Add edit (flask-based) close #57

--- engine/webserverflask.py
+++ engine/webserverflask.py
@@ -7,12 +7,13 @@ import sys
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print PATH
 sys.path.append(PATH)
-from engine.conf import PATH_TO_HTML, PATH_TO_TEMPLATE_HTML, PATH_TO_MD
+from engine.conf import PATH_TO_HTML, PATH_TO_TEMPLATE_HTML, PATH_TO_TEMPLATE, PATH_TO_MD
 print PATH_TO_TEMPLATE_HTML
 from engine import searcher
 from engine.postprocessing import add_head
 from flask import Flask, redirect, url_for, send_from_directory
 
+import subprocess
 import re
 
 app = Flask(__name__, static_url_path='')
@@ -23,13 +24,39 @@ def edit(note_title):
     os.system('open ' + PATH_TO_MD + ' ' + note_title)
     return 'edit note: %s' % note_title
 
+@app.route('/edit_header/<note_title>/<note_header>')
+def edit_header(note_title, note_header):
+    """
+    edit_header::cmd: cd /Users/magnus/Dropbox/geekbook/notes/ && /usr/bin/grep -n '# h1 Heading' test.md
+    edit_header::out: 11:# h1 Heading
+
+    Old: grep
+
+    grep -n 'May #2' *.md
+    lifebook.md:7:# May #
+    Open a note with your edit
+
+    http://stackoverflow.com/questions/3139970/open-a-file-at-line-with-filenameline-syntax
+    
+    ..warning: if two headers found there will be a problem ;-)
+    """
+    #grep -n 'May #2' *.md
+    cmd = "cd " + PATH_TO_MD + " && /usr/bin/grep -n '" + note_header + "' " + note_title + ".md"
+    print 'edit_header::cmd:', cmd
+    out = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).stdout.read()
+    print 'edit_header::out:', out
+    note_line = out.split(':')[0]
+    os.system('emacsclient +' + note_line + ' ' + PATH_TO_MD + '/' + note_title + '.md &')
+    
+    return redirect('/view/' + note_title + '.md#' + note_header.lstrip('#').strip().replace(' ', '-'))
+
 @app.route('/js/<path:path>')
 def send_js(path):
-    return send_from_directory(PATH_TO_TEMPLATE + os.sep + 'notes/js/' , path)
+    return send_from_directory(PATH_TO_TEMPLATE + os.sep + 'js/' , path)
 
 @app.route('/css/<path:path>')
 def send_css(path):
-    return send_from_directory(PATH_TO_TEMPLATE + os.sep + 'notes/css/' , path)
+    return send_from_directory(PATH_TO_TEMPLATE + os.sep + 'css/' , path)
 
 @app.route('/imgs/<path:path>')
 def send_img(path):
