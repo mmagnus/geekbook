10c2dcd192bc972a43d79e128da2934d90cd5545
mmagnus <m.magnus@zoho.com>
2017-07-05 11:08:16 +0200

    docs: too many 2.0 ;-)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'geekbook 2.0'
+project = u'geekbook'
 copyright = u'2013-2017, Marcin Magnus'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -202,7 +202,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'geekbook.tex', u'geekbook 2.0 Documentation',
+  ('index', 'geekbook.tex', u'geekbook Documentation',
    u'Marcin Magnus', 'manual'),
 ]
 
@@ -232,7 +232,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'geekbook', u'geekbook 2.0 Documentation',
+    ('index', 'geekbook', u'geekbook Documentation',
      [u'Marcin Magnus'], 1)
 ]
 
@@ -246,7 +246,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'geekbook', u'geekbook 2.0 Documentation',
+  ('index', 'geekbook', u'geekbook Documentation',
    u'Marcin Magnus', 'geekbook', 'One line description of project.',
    'Miscellaneous'),
 ]
