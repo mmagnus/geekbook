9dda31156fbf684ac2665a8575c0e25e3e8b261c
mmagnus <m.magnus@zoho.com>
2017-07-10 23:23:46 +0200

    geekbooksearch.py - proof of concept

---
+++ geekbooksearch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+
+import engine.searcher as sx
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('phrases')
+    parser.add_argument('-v', '--verbose', help='push to the server', action='store_true')
+    return parser
+    
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    
+    #print(sx.make_db())
+    sx.search_term(args.phrases)
+
+    
