5df2922d117d6262eacdcc238dc1dc638a0f1320
mmagnus <m.magnus@zoho.com>
2017-08-06 18:37:10 -0700

    auto-pep8

--- engine/searcher.py
+++ engine/searcher.py
@@ -23,9 +23,6 @@ Marcin Magnus
 init: 2012/10/05
 huge improv: 2012/11/08
 """
-debug = False
-
-
 import re
 import os
 import codecs
@@ -38,6 +35,9 @@ print(PATH)
 sys.path.append(PATH)
 from engine.conf import PATH_TO_HTML, PATH_TO_TEMPLATE_HTML, PATH_TO_MD
 
+debug = False
+
+
 def hightlight_text_in_html(phrase, text):
     """
     Replace NO_PUBKEY with a highlighted text '<span style="background-color:yellow">NO_PUBKEY</span>'
@@ -45,11 +45,14 @@ def hightlight_text_in_html(phrase, text):
     Normal phrase is used and .upper()
     """
     text = text.replace(phrase, '<span style="background-color:yellow">' + phrase + '</span>')
-    text = text.replace(phrase.upper(), '<span style="background-color:yellow">' + phrase.upper() + '</span>')
-    text = text.replace(phrase.title(), '<span style="background-color:yellow">' + phrase.title() + '</span>')
+    text = text.replace(
+        phrase.upper(), '<span style="background-color:yellow">' + phrase.upper() + '</span>')
+    text = text.replace(
+        phrase.title(), '<span style="background-color:yellow">' + phrase.title() + '</span>')
     return text
 
-def lsdir(directory = '/home/magnus/Desktop/', exclude_files_starting_with_dot=True, verbose=False):
+
+def lsdir(directory='/home/magnus/Desktop/', exclude_files_starting_with_dot=True, verbose=False):
     """
     magnus@maximus:~/workspace/myutil$ python test.py
     /home/magnus/Desktop/bookmarks-2010-10-15-present-bookmarks-nice.json
@@ -67,7 +70,7 @@ def lsdir(directory = '/home/magnus/Desktop/', exclude_files_starting_with_dot=T
     http://snippets.dzone.com/posts/show/644
 
     """
-    def walktree (top = ".", depthfirst = True):
+    def walktree(top=".", depthfirst=True):
         import stat
         names = os.listdir(top)
         if not depthfirst:
@@ -78,13 +81,13 @@ def lsdir(directory = '/home/magnus/Desktop/', exclude_files_starting_with_dot=T
             except os.error:
                 continue
             if stat.S_ISDIR(st.st_mode):
-                for (newtop, children) in walktree (os.path.join(top, name), depthfirst):
+                for (newtop, children) in walktree(os.path.join(top, name), depthfirst):
                     yield newtop, children
         if depthfirst:
             yield top, names
 
     f = []
-    for (basepath, children) in walktree(directory,False):
+    for (basepath, children) in walktree(directory, False):
         for child in children:
             if exclude_files_starting_with_dot:
                 if child.startswith('.'):
@@ -92,13 +95,15 @@ def lsdir(directory = '/home/magnus/Desktop/', exclude_files_starting_with_dot=T
                 else:
                     if verbose:
                         print os.path.join(basepath, child)
-                    f.append( os.path.join(basepath, child))
+                    f.append(os.path.join(basepath, child))
     return f
+
 
 class Header:
     """
     Header = # or ## or ### or ####
     """
+
     def __init__(self, name, level, md):
         """
         input:
@@ -106,37 +111,47 @@ class Header:
          - level: # = 1, ## = 2
          - md = travel
         """
-        
+
         self.name = name
         self.name_dashed = name.replace(' ', '-')
         self.level = level
         self.child = []
         self.note = ''
         self.md = md
+
     def add_child(self, header_obj):
         self.child.append(header_obj)
+
     def get_child(self):
         return self.child
+
     def get_note(self):
         return self.note
+
     def has_note(self):
         if self.note:
             return True
         else:
             return False
+
     def add_note(self, note):
         self.note = note
+
     def get_format(self, term):
         out = ''
-        out += '<h1 style="font-size:20px;color:black><a href="/view/' + self.md + '.html#' + self.name_dashed + '">' + self.name + '</a></h1>\n'
-        out += '<small style="color: #009933;">' + '<a href="/view/' + self.md + '.html#' + self.name_dashed + '">' + self.md + '</a>' + '</small>\n'
+        out += '<h1 style="font-size:20px;color:black><a href="/view/' + \
+            self.md + '.html#' + self.name_dashed + '">' + self.name + '</a></h1>\n'
+        out += '<small style="color: #009933;">' + '<a href="/view/' + self.md + \
+            '.html#' + self.name_dashed + '">' + self.md + '</a>' + '</small>\n'
         #        out += '<p>...' + myutilspy.hightlight_text_in_html(term, self.note).replace('\n','<br/>') + '...<p>\n'
-        out += '<pre>' + hightlight_text_in_html(term, self.note).replace('\n','<br/>') + '</pre>\n'
+        out += '<pre>' + hightlight_text_in_html(term,
+                                                 self.note).replace('\n', '<br/>') + '</pre>\n'
         out += '<div style="width:100%" class="hrDotted"></div>'
         return out
 
     def __repr__(self):
         return 'h' + str(self.level) + ': ' + self.name
+
 
 def next_lines_has_header_or_end_of_file(lines, no_lines, c, v=0):
     """
@@ -151,18 +166,21 @@ def next_lines_has_header_or_end_of_file(lines, no_lines, c, v=0):
     else:
         return False
 
+
 def replace_space_with_minus(text):
     replace = text.replace(' ', '-')
     return replace
 
-def make_headers_objects_for_md(filename, verbose=False,version2=True):
+
+def make_headers_objects_for_md(filename, verbose=False, version2=True):
     """
     filename = '/home/magnus/Dropbox/lb_v2/md/bioinfo::threading.md'
     """
     text = codecs.open(filename, mode="r", encoding="utf8").read()
     md = filename.replace(PATH_TO_MD, '')
-    md = re.sub('.md$', '', md)  # replace only .md at the very end    #.replace('.md','') ## md = bioinfo::threading
-    root = []  ## starting point of the structure
+    # replace only .md at the very end    #.replace('.md','') ## md = bioinfo::threading
+    md = re.sub('.md$', '', md)
+    root = []  # starting point of the structure
     lines = text.split('\n')
     no_lines = len(lines)
     list_h = ''
@@ -190,7 +208,7 @@ be fixed at some point. The problem is in the file: %s""" % filename)
             l = l.replace('### ', '')
             current_h3 = Header(l, 3, md)
             try:
-                current_h2.add_child(current_h3) ################ if not current2 then current1
+                current_h2.add_child(current_h3)  # if not current2 then current1
             except UnboundLocalError:
                 raise Exception(filename)
 
@@ -204,20 +222,22 @@ be fixed at some point. The problem is in the file: %s""" % filename)
                 raise Exception(filename)
             last_h = current_h4
         else:
-            note += ' ' + l + '\n' ## \n = not in one line!
+            note += ' ' + l + '\n'  # \n = not in one line!
             if next_lines_has_header_or_end_of_file(lines, no_lines, c):
                 try:
                     last_h.add_note(note)
                 except NameError:
                     pass
-                note = ''  ## reset the note variable
-    if verbose: print('root has following headers of # type: '.ljust(40), root)
+                note = ''  # reset the note variable
+    if verbose:
+        print('root has following headers of # type: '.ljust(40), root)
     #tb = PrettyTable(["r.name", "r.note", "r.level", "r.get_child()", "h3"])
     for r in root:
         row = [r.name, r.note[:50], r.level, r.get_child(), [x.get_child() for x in r.get_child()]]
-        if verbose: print(row)
-        #tb.add_row(row)
-    ## collect all h1
+        if verbose:
+            print(row)
+        # tb.add_row(row)
+    # collect all h1
     all_h = []
     for r in root:
         all_h.append(r)
@@ -227,9 +247,11 @@ be fixed at some point. The problem is in the file: %s""" % filename)
                 all_h.append(a)
     return all_h
 
+
 class Db():
     """
     """
+
     def __init__(self, ):
         """
         """
@@ -241,7 +263,7 @@ class Db():
         out2 = []
         #'/home/magnus/Dropbox/lb_v2/md/.#bash.md'
         for i in out:
-            if i.find('#')>-1 or i.find('~')>-1 or i.find('.org')>-1 or i.find('.git')>-1:
+            if i.find('#') > -1 or i.find('~') > -1 or i.find('.org') > -1 or i.find('.git') > -1:
                 pass
             else:
                 if i.endswith('.md'):
@@ -250,10 +272,11 @@ class Db():
         ## hack end ##
 
         all_headers = []
-        #print '<head><meta content="text/html; charset=UTF-8" http-equiv="content-type">'
-        #print "<link rel='stylesheet' href='/home/magnus/Dropbox/Public/lb/css/style.css' /></head>"
+        # print '<head><meta content="text/html; charset=UTF-8" http-equiv="content-type">'
+        # print "<link rel='stylesheet' href='/home/magnus/Dropbox/Public/lb/css/style.css' /></head>"
         for o in out:
-            if v: print(o)
+            if v:
+                print(o)
             all_headers.extend(make_headers_objects_for_md(o))
         self.all_headers = all_headers
 
@@ -268,25 +291,30 @@ class Db():
                 hits.append(h)
         hits_output = ''
         for hit in hits:
-            if v:print(hit.name)
-            if v:print(hit.note)
+            if v:
+                print(hit.name)
+            if v:
+                print(hit.note)
             h = hit.get_format(term)
             hits_output += h
-            if v:print
+            if v:
+                print
         return hits_output
 
 
 def make_db():
-    db = Db()                             
+    db = Db()
     print('searcher::making the db')
     db.collect_data()
-    pickle.dump(db, open(PATH + os.sep + "engine/searchdb.pickle", "wb" ))
+    pickle.dump(db, open(PATH + os.sep + "engine/searchdb.pickle", "wb"))
+
 
 def search_term(term):
     db = pickle.load(open(PATH + os.sep + "engine/searchdb.pickle", "rb"))
     return db.search(term)
 
-#main
+
+# main
 if __name__ == '__main__':
     term = '@test'
     print(make_db())
