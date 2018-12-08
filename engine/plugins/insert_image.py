a66b7648f684cd177c1ed4090191f10f9782a058
Marcin Magnus <mag_dex@o2.pl>
2018-12-08 19:12:24 +0100

    insert_image: add date of today when inserting a file

--- engine/plugins/insert_image.py
+++ engine/plugins/insert_image.py
@@ -2,6 +2,8 @@ import glob
 import os
 import shutil
 import sys
+import datetime
+
 
 def insert_image_in_md(text, d, td, IMG_PREFIX):
     """Go over each line and check if there is `ii`. If yes, then run insert_image function."""
@@ -13,7 +15,8 @@ def insert_image_in_md(text, d, td, IMG_PREFIX):
             changed = True
     return '\n'.join(ltext), changed # trigger compiles
 
-def insert_image(d = '/home/magnus/Desktop/*png', td = '/home/magnus/Dropbox/geekbook/notes/imgs/', IMG_PREFIX='imgs/'):
+
+def insert_image(d = '/home/magnus/Desktop/{*png,*jpg,*jpeg}', td = '/home/magnus/Dropbox/geekbook/notes/imgs/', IMG_PREFIX='imgs/'):
     """Check the latest file in d-rectory and copy it to t-arget d-rectory"""
     # make folder with imgs
     try:
@@ -24,8 +27,11 @@ def insert_image(d = '/home/magnus/Desktop/*png', td = '/home/magnus/Dropbox/gee
     newest = max(glob.iglob(d), key=os.path.getctime)
     # copy to img
     t = os.path.basename(newest.replace(' ','_'))
+    # add date
+    t = datetime.datetime.today().strftime('%y%m%d') + '_' + t
     shutil.move(newest, td + IMG_PREFIX + t)
     return '![](' + IMG_PREFIX  + t + ')'
+
 
 if __name__ == '__main__':
     insert_image()
