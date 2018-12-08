2caf17c86bab06675038cfef91b98247dc9d6e1d
Marcin Magnus <mag_dex@o2.pl>
2018-12-08 19:17:56 +0100

    find_files: update

--- engine/plugins/find_files.py
+++ engine/plugins/find_files.py
@@ -10,6 +10,7 @@ import platform
 import os
 import pandas as pd
 import commands
+import os
 
 import logging
 logger = logging.getLogger('geekbook')
@@ -20,19 +21,24 @@ JSON_DB = os.path.dirname(os.path.abspath(__file__)) + os.sep + 'find_file.json'
 def file_search(filename, verbose):
     """Search for filename. Returns dirname of the filename's path, and the full path.
 
+    181120 Test on my OSX. I might work on Linux as well. But this requires further
+    testing.
+
     170107 add cache. If the db is not found, create an empty pandas df
     and populate this df with append later. If the filename is not in the db
     run g/locate. Then, save the found path to the db (using pandas, via df, to json)"""
-
     # cache
     if os.path.isfile(JSON_DB):
         df = pd.read_json(JSON_DB, orient='records')
         #filename = 'x.pse'
         pathdf = df[df['fn'] == filename]['path']
-        if not pathdf.empty:
-            path = pathdf.to_string(index=False)
-            logger.info('find file [from the db]:' + filename)
+        # test if the file still exists there
+        path = pathdf.to_string(index=False)
+        if not pathdf.empty and os.path.isfile(path):
+            logger.info('[from the db]:' + filename)
             return os.path.dirname(path), path
+            # ok, here the function will finished if the results were cashed and
+            # file still exists there
     else:
         df = pd.DataFrame()
 
@@ -43,20 +49,22 @@ def file_search(filename, verbose):
         out = commands.getoutput('locate ' + filename)
     if platform.system() == "Darwin":
         # out = commands.getoutput('glocate ' + filename)
-        out = commands.getoutput('mdfind ' + filename)
+        out = commands.getoutput('mdfind -name ' + filename)
     first_hit = out.split('\n')[0]
     logger.info('# of hits ' + str(len(out.split('\n'))) + " " + out.replace('\n',', '))
     if not first_hit:
         logger.info('not found')
+        return ('Not found', '~~' + filename + '~~')
     else:
         logger.info('hit ' + first_hit)
 
     # update cache
     dffile = pd.DataFrame([[filename, first_hit],], columns=['fn', 'path'])
+    if verbose: print(df)
+    df[df['fn'] == filename]['path']
     df = df.append(dffile, ignore_index=True)
     # save to json
     df.to_json(JSON_DB, orient='records')
-    ##
     return os.path.dirname(first_hit), first_hit
 
 
@@ -71,7 +79,9 @@ def find_files(text, verbose=False):
             folderpath, filepath = file_search(filename, False)
             if folderpath:
                 if verbose: print '# file_finder.search()', output
-                output += l.replace('[ff:' + filename + ']',' <a href="' + folderpath + '"><code>[+]</code></a> ' + '<a target="_blank" href="/open' + filepath + '"> <span class="mantext">' + os.path.basename(filepath) + '</span></a>')
+                # remove for now folder link, it does not work @todo
+                #output += l.replace('[ff:' + filename + ']',' <a href="' + folderpath + '"><code>[+]</code></a> ' + '<a target="_blank" href="/open' + filepath + '"> <span class="mantext">' + os.path.basename(filepath) + '</span></a>')
+                output += l.replace('[ff:' + filename + ']', '<a target="_blank" href="/open' + filepath + '"> <span class="mantext">' + os.path.basename(filepath) + '</span></a>')
             else:
                 if verbose: print 'problem: can not find ' + filename
                 output += l.replace('[ff:' + filename + ']', '<span style="color:red">' + filename + '</span>')
